bb57bfd43cb22de8588fe40d4e1aea9c964927ef
huipengly <huipengly@gmail.com>
2018-05-07 16:53:28 +0800

    范围for正确写法，用4个元素数组的引用

--- cpp_primer/ch03/ex3_43.cpp
+++ cpp_primer/ch03/ex3_43.cpp
@@ -8,11 +8,11 @@ int main()
     int ia[3][4] = { 0 };
     // ver.1
     cout << "ver.1" << endl;
-    for (int (*row)[4] : ia)
+    for (int (&row)[4] : ia)
     {
-        for (int *col : row)
+        for (int col : row)
         {
-            cout << *col << " ";
+            cout << col << " ";
         }
         cout << endl;
     }
