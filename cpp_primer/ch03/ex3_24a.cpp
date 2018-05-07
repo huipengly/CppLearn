816a533ac80c464bd22db3a7fa957c456034f694
huipengly <huipengly@gmail.com>
2018-05-07 14:33:55 +0800

    结束输入数字使用ctrl+d

--- cpp_primer/ch03/ex3_24a.cpp
+++ cpp_primer/ch03/ex3_24a.cpp
@@ -10,10 +10,18 @@ int main()
 {
     int read_int{0};
     vector<int> ivec;
-    while (cin >> read_int)//要输入非数字才会跳出，有问题
+    // for (int buffer; cin >> buffer; ivec.push_back(buffer));//github上的写法
+    cout << "please input some number!" << endl;
+    while (cin >> read_int)//要输入非数字才会跳出，有问题//linux下面输入ctrl+d作为文件结束符
     {
         ivec.push_back(read_int);
     }
+    if(ivec.size() < 2)
+    {
+        cout << "please input 2 number at least" << endl;
+        return -1;
+    }
+    cout << "result" << endl;
     for (auto it = ivec.begin(); it != ivec.end() - 1; ++it)
     {
         cout << *it + *(it + 1) << endl;
