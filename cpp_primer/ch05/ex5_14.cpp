6d1e7fd22e5a03b47c815b539821cf35dd516d63
huipengly <huipengly@gmail.com>
2018-05-08 18:24:15 +0800

    5.14题目要求的是输出最大的，之前读题错误。并加入GitHub上的答案，用来对比

--- cpp_primer/ch05/ex5_14.cpp
+++ cpp_primer/ch05/ex5_14.cpp
@@ -9,26 +9,33 @@ int main()
 {
     string str;
     string pre_str;
-    int counter = 1;
+    string max_str;
+    int counter = 0;
+    int max_counter = 0;
     while (cin >> str)
     {
-        if (str == pre_str)//当第一次有相等时，说明已经有一个了，加完之后应该是2个
+        if (str == pre_str)
         {
             ++counter;
         }
         else
         {
-            if (counter != 1)
+            if(counter > max_counter)
             {
-                cout << pre_str << " : " << counter << endl;
+                max_str = pre_str;
+                max_counter = counter + 1;//当第一次有相等时，说明已经和前一个像等了，所以总数应该加一个
             }
-            counter = 1;
+            counter = 0;
         }
         pre_str = str;
     }
-    if(counter != 1)//最后如果有连续的话，while不会处理，这里处理
+    if(max_counter != 0)
     {
-        cout << pre_str << " : " << counter << endl;
+        cout << max_str << " : " << max_counter << endl;
+    }
+    else
+    {
+        cout << "There's no duplicated string." << endl;
     }
     return 0;
 }
