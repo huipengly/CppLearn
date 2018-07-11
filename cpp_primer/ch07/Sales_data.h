eee7f3e1964c1a22ae5e830b311c5cf2a248eddb
huipengly <huipengly@gmail.com>
2018-07-11 18:34:43 +0800

    完成练习7.41

--- cpp_primer/ch07/Sales_data.h
+++ cpp_primer/ch07/Sales_data.h
@@ -4,7 +4,7 @@
 #include <string>
 using std::string;
 #include <iostream>
-using std::istream; using std::ostream;
+using std::istream; using std::ostream; using std::cout;
 
 class Sales_data;                                      //先声明这个类名，给下面三个声明使用。
 istream &read(istream&, Sales_data&);                   //read的声明要在类定义前，不然无法调用
@@ -17,11 +17,11 @@ friend istream &read(istream&, Sales_data&);
 friend ostream &print(ostream&, const Sales_data&);
 friend Sales_data add(const Sales_data&, const Sales_data&);
 public:
-    Sales_data() = default;
-    Sales_data(istream& is) { read(is, *this); }
     Sales_data(const string &s, const unsigned &n, const double &p) : 
-                bookNo(s), units_sold(n), revenue(p * n) { }
-    Sales_data(const string& s) : bookNo(s) { }
+                bookNo(s), units_sold(n), revenue(p * n) { cout << "Sales_data(const string &s, const unsigned &n, const double &p)\n"; }
+    Sales_data() : Sales_data("", 0, 0) { cout << "Sales_data()\n"; }
+    Sales_data(const string& s) : Sales_data(s, 0, 0) { cout << "Sales_data(const string& s)\n"; }
+    Sales_data(istream& is) : Sales_data() { cout << "Sales_data(istream& is)\n"; read(is, *this); }
 
     string isbn() const { return bookNo; }
     Sales_data& combine(const Sales_data&);
