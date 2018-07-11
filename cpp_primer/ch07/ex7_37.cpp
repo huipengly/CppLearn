0f15903175faa1609fd5ea1d38cf53cfaf4b01bf
huipengly <huipengly@gmail.com>
2018-07-11 18:34:27 +0800

    研究练习3.37

---
+++ cpp_primer/ch07/ex7_37.cpp
@@ -0,0 +1,12 @@
+#include "Sales_data.h"
+#include <iostream>
+using std::cout; using std::endl;
+
+int main()
+{
+    Sales_data next;
+
+    print(cout, next);
+    cout << endl;
+    return 0;
+}
