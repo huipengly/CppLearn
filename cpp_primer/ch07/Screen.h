b60282bdb96e1aa85226fc4147914f91a860cac3
huipengly <huipengly@gmail.com>
2018-07-11 10:14:02 +0800

    将Screen类改回正确的定义

--- cpp_primer/ch07/Screen.h
+++ cpp_primer/ch07/Screen.h
@@ -11,11 +11,11 @@ public:
     Screen(pos wd, pos ht, char c = ' ') : height(ht), width(wd), contents(ht * wd, c) {}
     char get() const { return contents[cursor]; }
     char get(pos r, pos c) const { return contents[ r * width + c]; }
-    Screen move(pos r, pos c) { cursor = r * width + c; return *this; }
-    Screen set(char c) { contents[cursor] = c; return *this; }
-    Screen set(pos r, pos col, char ch) { contents[r * width + col] = ch; return *this; }
-    Screen display(ostream &os) { do_display(os); return *this; }
-    const Screen display(ostream &os) const { do_display(os); return *this; }
+    Screen &move(pos r, pos c) { cursor = r * width + c; return *this; }
+    Screen &set(char c) { contents[cursor] = c; return *this; }
+    Screen &set(pos r, pos col, char ch) { contents[r * width + col] = ch; return *this; }
+    Screen &display(ostream &os) { do_display(os); return *this; }
+    const Screen &display(ostream &os) const { do_display(os); return *this; }
 
 private:
     void do_display(ostream &os) const { os << contents; }
