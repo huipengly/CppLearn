406299e17623013d1f739345fd71fa843ac88ac5
huipengly <huipengly@gmail.com>
2018-07-16 18:56:14 +0800

    完善习题8.2

--- cpp_primer/ch08/ex8_2.cpp
+++ cpp_primer/ch08/ex8_2.cpp
@@ -9,17 +9,20 @@ istream &print_stream(istream &is)
     while(!is.eof())
     {
         is >> s;
-        cout << s;
+        cout << s << endl;
     }
 
+    cout << is.rdstate() << endl;
     is.clear();
+    cout << is.rdstate() << endl;
 
     return is;
 }
 
 int main()
 {
-    print_stream(cin);
+    istream &is = print_stream(cin);        //必须声明为引用，因为IO不能拷贝或赋值
+    cout << is.rdstate() << endl;
 
     return 0;
 }
