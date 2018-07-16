d475e0196256de659fa5892b270c0d4cc690997f
huipengly <huipengly@gmail.com>
2018-07-16 19:59:28 +0800

    完成练习8.8

---
+++ cpp_primer/ch08/ex8_8.cpp
@@ -0,0 +1,34 @@
+#include "../ch07/Sales_data.h"
+#include <iostream>
+using std::cin; using std::cout; using std::endl; using std::cerr;
+#include <fstream>
+using std::ifstream; using std::ofstream;
+
+int main(int argc, char *argv[])
+{
+    ifstream in(argv[1]);
+    ofstream out(argv[2], ofstream::app);
+    Sales_data total;
+    if (read(in, total))
+    {
+        Sales_data trans;
+        while (read(in, trans))
+        {
+            if (total.isbn() == trans.isbn())
+            {
+                total.combine(trans);
+            }
+            else
+            {
+                print(out, total) << endl;
+                total = trans;
+            }
+        }
+        print(out, total) << endl;
+    }
+    else
+    {
+        cerr << "No data?!" << endl;
+    }
+    return 0;
+}
