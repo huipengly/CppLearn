0b0c2977217e92930331a0a8928ed4a473d52113
huipengly <huipengly@gmail.com>
2018-07-18 13:36:19 +0800

    更改练习9.14的list初始化方式

--- cpp_primer/ch09/ex9_14.cpp
+++ cpp_primer/ch09/ex9_14.cpp
@@ -9,13 +9,15 @@ using std::cout; using std::endl;
 
 int main()
 {
-    list<char *> list_of_char;
+    // list<char *> list_of_char;
     vector<string> vector_of_string;
 
-    char s1[] = "hello", s2[] = "world", s3[] = "!";
-    list_of_char.push_back(s2);
-    list_of_char.push_front(s1);
-    list_of_char.push_back(s3);
+    // char s1[] = "hello", s2[] = "world", s3[] = "!";
+    // list_of_char.push_back(s2);
+    // list_of_char.push_front(s1);
+    // list_of_char.push_back(s3);
+
+    std::list<const char*> list_of_char{ "hello", "world", "!" };
 
     vector_of_string.assign(list_of_char.begin(), list_of_char.end());
 
