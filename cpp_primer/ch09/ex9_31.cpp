51df4a4ca481b27f112e22961001bf1eef3da63c
huipengly <huipengly@gmail.com>
2018-07-20 09:08:24 +0800

    验证练习9.31

---
+++ cpp_primer/ch09/ex9_31.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout; using std::endl;
+#include <list>
+using std::list;
+
+int main()
+{
+    list<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto iter = vi.begin();
+    while (iter != vi.end())
+    {
+        if (*iter % 2)
+        {
+            iter = vi.insert(iter, *iter);
+            ++iter;++iter;
+        }
+        else
+        {
+            iter = vi.erase(iter);
+        }
+    }
+
+    for (const auto &i : vi)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
