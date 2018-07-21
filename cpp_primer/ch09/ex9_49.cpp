b39e03104dae5b115c656ba8fd08ff25117300a8
huipengly <huipengly@gmail.com>
2018-07-21 16:45:11 +0800

    完成练习9.49

---
+++ cpp_primer/ch09/ex9_49.cpp
@@ -0,0 +1,36 @@
+#include <string>
+using std::string;
+#include <iostream>
+using std::cout; using std::endl; using std::cerr; using std::istream;
+#include <fstream>
+using std::ifstream;
+
+string ascender = "bdfhkIt";
+string descender = "gjpqy";
+
+void print_withou_word_with_ascender_and_descender(istream &is)
+{
+    string word;
+    while (is >> word)
+    {
+        if (word.find_first_of(ascender) == string::npos && word.find_first_of(descender) == string::npos)
+        {
+            cout << word << ' ';
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ifstream ifs("data_for_ex9_49.txt");
+    if (ifs)
+    {
+        print_withou_word_with_ascender_and_descender(ifs);
+    }
+    else
+    {
+        cerr << "open file failed!" << endl;
+    }
+    return 0;
+}
