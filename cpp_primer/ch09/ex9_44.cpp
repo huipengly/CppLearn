6a7188c9757a3b0dac96eecdaa432c1881a94da1
huipengly <huipengly@gmail.com>
2018-07-21 14:58:42 +0800

    完成练习9.44

---
+++ cpp_primer/ch09/ex9_44.cpp
@@ -0,0 +1,27 @@
+#include <string>
+using std::string;
+#include <iostream>
+using std::cout; using std::endl;
+
+void replace_string(string &s, const string &old_val, const string &new_val)
+{
+	for (auto i = 0; i != s.size() - old_val.size(); ++i)
+	{
+		string s2 = string(s, i, old_val.size());
+		if (s2 == old_val)
+		{
+			s.replace(i, old_val.size(), new_val);
+		}
+	}
+}
+
+int main()
+{
+	string s1{ "i tho thru aa!" };
+	cout << s1 << endl;
+	replace_string(s1, "tho", "though");
+	cout << s1 << endl;
+	replace_string(s1, "thru", "through");
+	cout << s1 << endl;
+	return 0;
+}
