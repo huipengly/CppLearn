6605aa7782080f4bdbbd62f44c6eba47f4e55ae5
huipengly <huipengly@gmail.com>
2018-07-23 20:31:28 +0800

    练习10.3的功能独立为函数

--- cpp_primer/ch10/ex10_9.cpp
+++ cpp_primer/ch10/ex10_9.cpp
@@ -14,12 +14,22 @@ using std::cout; using std::endl;
 #include <algorithm>
 using std::sort; using std::unique;
 
+/**
+ * @brief 删除重复单词
+ * 
+ * @param word 句子，每个string存储一个单词
+ */
+void elim_dumps(vector<string> &word)
+{
+    sort(word.begin(), word.end());
+    auto end_unique = unique(word.begin(), word.end());
+    word.erase(end_unique, word.end());
+}
+
 int main()
 {
     vector<string> svec = {"the", "quick", "red", "fox", "jump", "over", "the", "slow", "red", "turtle"};
-    sort(svec.begin(), svec.end());
-    auto end_unique = unique(svec.begin(), svec.end());
-    svec.erase(end_unique, svec.end());
+    elim_dumps(svec);
     for (auto str : svec)
     {
         cout << str << " ";
