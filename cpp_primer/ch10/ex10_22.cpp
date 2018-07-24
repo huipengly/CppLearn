f377e8469f75c523dad67354ad643dfc5ffdb465
huipengly <huipengly@gmail.com>
2018-07-24 11:19:11 +0800

    修改练习10.22的函数名，并加入注释

--- cpp_primer/ch10/ex10_22.cpp
+++ cpp_primer/ch10/ex10_22.cpp
@@ -16,19 +16,34 @@ using std::cout; using std::endl;
 #include <functional>
 using std::placeholders::_1; using std::bind;
 
-bool smaller_than_size(const string &str, string::size_type sz)
+/**
+ * @brief 检查给定字符串长度
+ * 
+ * @param str 给定字符串
+ * @param sz 长度
+ * @return true 给定字符串长度小于等于sz
+ * @return false 给定字符串长度大于sz
+ */
+bool check_size(const string &str, string::size_type sz)
 {
     return str.size() <= sz;
 }
 
-int check_size(const vector<string> &words, string::size_type sz)
+/**
+ * @brief 检测句子中有多少单词长度小于等于给定长度
+ * 
+ * @param words 句子
+ * @param sz 长度
+ * @return int 小于等于给定长度单词的数目
+ */
+int smaller_than_size(const vector<string> &words, string::size_type sz)
 {
-    return count_if(words.begin(), words.end(), bind(smaller_than_size, _1, sz));
+    return count_if(words.begin(), words.end(), bind(check_size, _1, sz));
 }
 
 int main()
 {
     vector<string> svec = {"the", "quick", "red", "fox", "jump", "over", "the", "slow", "red", "turtle"};
-    cout << check_size(svec, 6) << endl;
+    cout << smaller_than_size(svec, 6) << endl;
     return 0;
 }
