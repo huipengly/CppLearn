dc4822c84cd5b7393c363d4ef728d20e1ff864c6
huipengly <huipengly@gmail.com>
2018-08-01 13:33:55 +0800

    测试练习11.10

---
+++ cpp_primer/ch11/ch11_10.cpp
@@ -0,0 +1,13 @@
+#include <map>
+using std::map;
+#include <vector>
+using std::vector;
+#include <list>
+using std::list;
+
+int main()
+{
+    map<vector<int>::iterator, int> aa;
+    map<list<int>::iterator, int> bb;
+    return 0;
+}
