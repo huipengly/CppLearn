0d5d43314e0b32fc28dc0726cc7e1ea566b8cd1f
huipengly <huipengly@gmail.com>
2018-09-03 08:15:03 +0800

    完成练习13.22

---
+++ cpp_primer/ch13/ex13_22.h
@@ -0,0 +1,23 @@
+#ifndef _HAS_PTR_H_
+#define _HAS_PTR_H_
+#include <string>
+
+class HasPtr
+{
+public:
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
+    HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) {}                  //!< 拷贝构造函数
+    HasPtr& operator=(const HasPtr &hp)                                                 //!< 拷贝赋值函数
+    { 
+        delete ps;
+        ps = hp.ps;
+        i = hp.i;
+        return *this;
+    }
+    ~HasPtr() { delete ps; }                                                            //!< 析构函数
+private:
+    std::string *ps;
+    int i;
+};
+
+#endif
