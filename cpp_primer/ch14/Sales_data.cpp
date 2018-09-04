a41c36cf765118809f5c8fe6408d43a0fb9a1931
huipengly <huipengly@gmail.com>
2018-09-04 18:39:30 +0800

    完成练习14.2

---
+++ cpp_primer/ch14/Sales_data.cpp
@@ -0,0 +1,31 @@
+#include "Sales_data.h"
+
+using std::endl;
+
+Sales_data& Sales_data::operator+=(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum += rhs;
+    return sum;
+}
+
+istream &operator>>(istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold * price;
+    return is;
+}
+
+ostream &operator<<(ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+    return os;
+}
---
+++ cpp_primer/ch14/Sales_data.h
@@ -0,0 +1,38 @@
+#ifndef _SALES_DATA_H_
+#define _SALES_DATA_H_
+
+#include <string>
+using std::string;
+#include <iostream>
+using std::istream; using std::ostream; using std::cout;
+
+class Sales_data;                                      //先声明这个类名，给下面三个声明使用。
+istream & operator>>(istream&, Sales_data&);           //read的声明要在类定义前，不然无法调用
+ostream & operator<<(ostream&, const Sales_data&);
+Sales_data operator+(const Sales_data&, const Sales_data&);
+
+class Sales_data
+{
+    friend istream & operator>>(istream&, Sales_data&);
+    friend ostream & operator<<(ostream&, const Sales_data&);
+    friend Sales_data operator+(const Sales_data&, const Sales_data&);
+public:
+    Sales_data(const string &s, const unsigned &n, const double &p) : 
+                bookNo(s), units_sold(n), revenue(p * n) { cout << "Sales_data(const string &s, const unsigned &n, const double &p)\n"; }
+    Sales_data() : Sales_data("", 0, 0) { cout << "Sales_data()\n"; }
+    Sales_data(const string& s) : Sales_data(s, 0, 0) { cout << "Sales_data(const string& s)\n"; }
+    Sales_data(istream& is) : Sales_data() { cout << "Sales_data(istream& is)\n"; read(is, *this); }
+
+    string isbn() const { return bookNo; }
+    Sales_data& operator+=(const Sales_data&);
+
+private:
+    double avg_price() const { return revenue / units_sold; }
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+
+#endif
