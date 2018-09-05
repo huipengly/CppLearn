b9e4162657b536a7f2aa3aa09ed39cc2c1ab701b
huipengly <huipengly@gmail.com>
2018-09-05 19:15:46 +0800

    修改调用标准库函数对象的方法

--- cpp_primer/ch14/ex14_43.cpp
+++ cpp_primer/ch14/ex14_43.cpp
@@ -8,12 +8,11 @@ using namespace std::placeholders;
 int main()
 {
     vector<int> ivec{2, 4, 8, 16};
-    std::modulus<int> mod;
-    if (none_of(ivec.begin(), ivec.end(), bind(mod, 1024, _1)))
+    if (none_of(ivec.begin(), ivec.end(), bind(modulus<int>(), 1024, _1)))
     {
         cout << "1024是" << endl;
     }
-    if (none_of(ivec.begin(), ivec.end(), bind(mod, 1025, _1)))
+    if (none_of(ivec.begin(), ivec.end(), bind(modulus<int>(), 1025, _1)))
     {
         cout << "1025是" << endl;
     }
