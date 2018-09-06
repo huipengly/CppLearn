051ba1f0d698c11dc7b7258009eae51e6d41664c
huipengly <huipengly@gmail.com>
2018-09-06 14:00:56 +0800

    完成练习15.17

--- cpp_primer/ch15/ex15_17.cpp
+++ cpp_primer/ch15/ex15_17.cpp
@@ -14,6 +14,10 @@ int main()
     Quote q("978-7-121-15535-2", 20);
     BulkQuote bq("978-7-121-15535-2", 20, 3, 0.2);
     LimitQuote lq("978-7-121-15535-2", 20, 2, 0.1);
+
+    // error: cannot declare variable ‘dq’ to be of abstract type ‘DiscQuote’
+    // DiscQuote dq("978-7-121-15535-2", 20, 2, 0.1);
+
     print_total(cout, q, 2);
     print_total(cout, bq, 2);
     print_total(cout, bq, 4);
