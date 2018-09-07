e2b16dfbea498d057a98ead466751f882257f56a
huipengly <huipengly@gmail.com>
2018-09-07 11:08:24 +0800

    添加拷贝控制成员

--- cpp_primer/ch15/Quote.cpp
+++ cpp_primer/ch15/Quote.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
 #include "Quote.h"
 
+Quote &Quote::operator=(const Quote &rhs)
+{
+    bookNo = rhs.bookNo;
+    price = rhs.price;
+    std::cout << "Quote &operator=(const Quote &)" << std::endl;
+    return *this;
+}
+
+Quote &Quote::operator=(Quote &&rhs)
+{
+    bookNo = std::move(rhs.bookNo);
+    price = std::move(rhs.price);
+    std::cout << "Quote &operator=(Quote &&)" << std::endl;
+    return *this;
+}
+
 void Quote::debug()
 {
     std::cout << "bookNo = " << bookNo << std::endl;
     std::cout << "price = " << price << std::endl;
+}
+
+DiscQuote &DiscQuote::operator=(const DiscQuote &rhs)
+{
+    Quote::operator=(rhs);
+    quantity = rhs.quantity;
+    discount = rhs.discount;
+    std::cout << "DiscQuote &operator=(const DiscQuote &)" << std::endl;
+    return *this;
+}
+
+DiscQuote &DiscQuote::operator=(DiscQuote &&rhs)
+{
+    Quote::operator=(std::move(rhs));
+    quantity = std::move(rhs.quantity);
+    discount = std::move(rhs.discount);
+    std::cout << "DiscQuote &operator=(DiscQuote &&)" << std::endl;
+    return *this;
+}
+
+BulkQuote &BulkQuote::operator=(const BulkQuote &rhs)
+{
+    DiscQuote::operator=(rhs);
+    std::cout << "BulkQuote &operator=(const BulkQuote &)" << std::endl;
+}
+
+BulkQuote &BulkQuote::operator=(BulkQuote &&rhs)
+{
+    DiscQuote::operator=(std::move(rhs));
+    std::cout << "BulkQuote &operator=(BulkQuote &&)" << std::endl;
 }
 
 double BulkQuote::net_price(size_t n) const
@@ -21,6 +67,17 @@ void BulkQuote::debug()
     std::cout << "discount = " << discount << std::endl;
 }
 
+LimitQuote &LimitQuote::operator=(const LimitQuote &rhs)
+{
+    DiscQuote::operator=(rhs);
+    std::cout << "LimitQuote &operator=(const LimitQuote &)" << std::endl;
+}
+
+LimitQuote &LimitQuote::operator=(LimitQuote &&rhs)
+{
+    DiscQuote::operator=(std::move(rhs));
+    std::cout << "LimitQuote &operator=(LimitQuote &&)" << std::endl;
+}
 
 double LimitQuote::net_price(size_t n) const
 {
--- cpp_primer/ch15/Quote.h
+++ cpp_primer/ch15/Quote.h
@@ -3,20 +3,28 @@
  * 
  * @file Quote.h
  * @author huipengly
- * @date 2018-09-06
+ * @date 2018-09-07
  */
 #ifndef _QUOTE_H_
 #define _QUOTE_H_
 #include <string>
+#include <iostream>
 
 class Quote
 {
 public:
     Quote() = default;
     Quote(const std::string &s, double p) : bookNo(s), price(p) {}
+    Quote(const Quote &q) : bookNo(q.bookNo), price(q.price)
+        { std::cout << "Quote(const Quote &q)" << std::endl;}
+    Quote(Quote &&q) : bookNo(std::move(q.bookNo)), price(std::move(q.price))
+        { std::cout << "Quote(Quote &&q)" << std::endl;}
+    Quote &operator=(const Quote &);
+    Quote &operator=(Quote &&);
     std::string isbn() const { return bookNo; }
     virtual double net_price(std::size_t sz) const { return price * sz; }
     virtual void debug();
+    virtual ~Quote() = default;
 private:
     std::string bookNo;
 protected:
@@ -29,6 +37,12 @@ public:
     DiscQuote() = default;
     DiscQuote(const std::string &book, double sales_price, std::size_t qty, double disc) :
         Quote(book, sales_price), quantity(qty), discount(disc) {}
+    DiscQuote(const DiscQuote &d) : Quote(d), quantity(d.quantity), discount(d.discount)
+        { std::cout << "DiscQuote(const DiscQuote &d)" << std::endl;}
+    DiscQuote(DiscQuote &&d) : Quote(std::move(d)), quantity(std::move(d.quantity)), discount(std::move(d.discount))
+        { std::cout << "DiscQuote(DiscQuote &&d)" << std::endl;}
+    DiscQuote &operator=(const DiscQuote &);  // 因为纯虚类，所以不能实例对象，返回值不能为右值，必须为引用
+    DiscQuote &operator=(DiscQuote &&);
     virtual double net_price(std::size_t n) const = 0;
 protected:
     std::size_t quantity = 0;
@@ -38,8 +52,15 @@ protected:
 class BulkQuote : public DiscQuote
 {
 public:
+    // using DiscQuote::DiscQuote;
     BulkQuote(const std::string &book, double sales_price, std::size_t qty, double disc) :
         DiscQuote(book, sales_price, qty, disc) {}
+    BulkQuote(const BulkQuote &b) : DiscQuote(b) 
+        { std::cout << "BulkQuote(const BulkQuote &b)" << std::endl;}
+    BulkQuote(BulkQuote &&b) : DiscQuote(std::move(b)) 
+        { std::cout << "BulkQuote(BulkQuote &&b)" << std::endl;}
+    BulkQuote &operator=(const BulkQuote &);
+    BulkQuote &operator=(BulkQuote &&);
     double net_price(std::size_t) const override;
     void debug() override;
 };
@@ -47,8 +68,15 @@ public:
 class LimitQuote : public DiscQuote
 {
 public:
+    // using DiscQuote::DiscQuote;
     LimitQuote(const std::string &book, double sales_price, std::size_t qty, double disc) :
         DiscQuote(book, sales_price, qty, disc) {}
+    LimitQuote(const LimitQuote &l) : DiscQuote(l) 
+        { std::cout << "LimitQuote(const LimitQuote &l)" << std::endl;}
+    LimitQuote(LimitQuote &&l) : DiscQuote(std::move(l)) 
+        { std::cout << "LimitQuote(LimitQuote &&l)" << std::endl;}
+    LimitQuote &operator=(const LimitQuote &);
+    LimitQuote &operator=(LimitQuote &&);
     double net_price(std::size_t) const override;
     void debug() override;
 };
