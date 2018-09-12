ed6b34ffc7a5bec37f13a3934a9ac30b1fd730c5
huipengly <huipengly@gmail.com>
2018-09-12 09:17:18 +0800

    之前理解错题目了，重新完成练习16.14

--- cpp_primer/ch16/Screen.h
+++ cpp_primer/ch16/Screen.h
@@ -2,59 +2,54 @@
 #define _SCREEN_H_
 
 #include <string>
-using std::string;
 #include <iostream>
-using std::ostream;
 #include <vector>
-using std::vector;
 
-template <typename> class Screen;
+template <std::string::size_type, std::string::size_type> class Screen;
 
-template <typename T>                                               // Hints:调用模板类，所以这个类也是个模板类
+template <std::string::size_type H, std::string::size_type W>
 class WindowMgr
 {
 public:
-    using ScreenIndex = typename vector<Screen<T>>::size_type;      // Hints:调用模板类要加<T>
+    using ScreenIndex = typename std::vector<Screen<H, W>>::size_type;  // Hints:类型名需要加typename
     void clear(ScreenIndex);
 private:
-    vector<Screen<T>> screens;
+    //std::vector<Screen> screens{Screen(24, 80, ' ')};      //报错，不能在定义Screen之前使用其构造函数
+    std::vector<Screen<H, W>> screens;
 };
 
-template <typename T>
+template <std::string::size_type H, std::string::size_type W>
 class Screen
 {
-    friend void WindowMgr<T>::clear(WindowMgr<T>::ScreenIndex);     // Hints:模板类的函数，需要在域说明符前的类加<T>
+    friend void WindowMgr<H, W>::clear(WindowMgr<H, W>::ScreenIndex);   // Hints: 后面的ScreenIndex也需要定义域符，因为需要指定实例化的WindowMgr
 
 public:
-    using pos = T;      // Hints:T前不加typename
+    using pos = std::string::size_type;
     Screen() = default;
-    Screen(pos wd, pos ht, char c = ' ') : height(ht), width(wd), contents(ht * wd, c) {}
+    Screen(char c = ' ') : contents(H * W, c) {}
     char get() const { return contents[cursor]; }
-    char get(pos r, pos c) const { return contents[ r * width + c]; }
-    Screen &move(pos r, pos c) { cursor = r * width + c; return *this; }
+    char get(pos r, pos c) const { return contents[ r * W + c]; }
+    Screen &move(pos r, pos c) { cursor = r * W + c; return *this; }
     Screen &set(char c) { contents[cursor] = c; return *this; }
-    Screen &set(pos r, pos col, char ch) { contents[r * width + col] = ch; return *this; }
-    Screen &display(ostream &os) { do_display(os); return *this; }
-    const Screen &display(ostream &os) const { do_display(os); return *this; }
-    pos size() { return width * height; }
+    Screen &set(pos r, pos col, char ch) { contents[r * W + col] = ch; return *this; }
+    Screen &display(std::ostream &os) { do_display(os); return *this; }
+    const Screen &display(std::ostream &os) const { do_display(os); return *this; }
+    pos size() { return W * H; }
 
 private:
-    void do_display(ostream &os) const { os << contents; }
-    pos height;         //高度
-    pos width;          //宽度
+    void do_display(std::ostream &os) const { os << contents; }
     pos cursor;         //光标
 
-    string contents;
+    std::string contents;
 };
 
-template <typename T> inline
-void WindowMgr<T>::clear(ScreenIndex screen_index)      //卸载后面是因为需要Screen完成定义
+template <std::string::size_type H, std::string::size_type W> inline
+void WindowMgr<H, W>::clear(ScreenIndex screen_index)     // 需要定义为WindowMgr<H, W> //写在后面是因为需要Screen完成定义
 {
-    if (screen_index < screens.size())                  //安全性判断
+    if (screen_index < screens.size())              //安全性判断
     {
-        Screen<T> &s = screens[screen_index];           //增加别名，提高程序可读性
-                                                        // 调用模板类，需要用Screen<T>
-        s.contents = string(s.width * s.height, ' ');
+        Screen<H, W> &s = screens[screen_index];          //增加别名，提高程序可读性
+        s.contents = std::string(s.W * s.H, ' ');
     }
 }
 
