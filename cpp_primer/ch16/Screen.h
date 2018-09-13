3304d8b62f276598b74daa51060f87b8dfd23518
huipengly <huipengly@gmail.com>
2018-09-13 10:31:04 +0800

    完成练习16.15

--- cpp_primer/ch16/Screen.h
+++ cpp_primer/ch16/Screen.h
@@ -6,30 +6,17 @@
 #include <vector>
 
 template <std::string::size_type, std::string::size_type> class Screen;
-template <std::string::size_type X, std::string::size_type Y>
-std::istream &operator>>(const std::istream &, const Screen<X, Y> &);
+template <std::string::size_type H, std::string::size_type W>
+    std::istream &operator>>(std::istream &, Screen<H, W> &);           // Hints:这里函数参数是没有const的！！！！！
 
 template <std::string::size_type H, std::string::size_type W>
-class WindowMgr
-{
-public:
-    using ScreenIndex = typename std::vector<Screen<H, W>>::size_type;  // Hints:类型名需要加typename
-    void clear(ScreenIndex);
-private:
-    //std::vector<Screen> screens{Screen(24, 80, ' ')};      //报错，不能在定义Screen之前使用其构造函数
-    std::vector<Screen<H, W>> screens;
-};
-
-// template <std::string::size_type H, std::string::size_type W>
-// std::ostream &operator<<(const std::ostream &, const Screen<H, W> &);
-
+    std::ostream &operator<<(std::ostream &, const Screen<H, W> &);
 
 template <std::string::size_type H, std::string::size_type W>
 class Screen
 {
-    friend void WindowMgr<H, W>::clear(WindowMgr<H, W>::ScreenIndex);   // Hints: 后面的ScreenIndex也需要定义域符，因为需要指定实例化的WindowMgr
-    // friend std::ostream &operator<<<H, W>(const std::ostream &, const Screen<H, W> &);
-    template <std::string::size_type X, std::string::size_type Y> friend std::istream &operator>>(const std::istream &, const Screen<X, Y> &);      // 任何一个实例均为friend
+    friend std::ostream &operator<<<H, W>(std::ostream &, const Screen<H, W> &);
+    friend std::istream &operator>><H, W>(std::istream &, Screen<H, W> &);
 
 public:
     using pos = std::string::size_type;
@@ -51,40 +38,26 @@ private:
     std::string contents;
 };
 
-template <std::string::size_type H, std::string::size_type W> inline
-void WindowMgr<H, W>::clear(ScreenIndex screen_index)     // 需要定义为WindowMgr<H, W> //写在后面是因为需要Screen完成定义
+template <std::string::size_type H, std::string::size_type W>
+std::ostream &operator<<(std::ostream &os, const Screen<H, W> &screen)
 {
-    if (screen_index < screens.size())              //安全性判断
+    for (auto h = 0; h != H; ++h)
     {
-        Screen<H, W> &s = screens[screen_index];          //增加别名，提高程序可读性
-        s.contents = std::string(s.W * s.H, ' ');
+        os << screen.contents.substr(h * W, W) << '\n';
     }
+    return os;
 }
 
-// template <std::string::size_type H, std::string::size_type W>
-// std::ostream &operator<<(const std::ostream &os, const Screen<H, W> &screen)
-// {
-//     os << screen.contents;
-//     return os;
-// }
-
-template <std::string::size_type X, std::string::size_type Y>
-std::istream &operator>>(std::istream &is, Screen<X, Y> &screen)
+template <std::string::size_type H, std::string::size_type W>
+std::istream &operator>>(std::istream &is, Screen<H, W> &screen)
 {
-
-            char a;
-            is>>a;
-            std::string temp(X*Y, a);
-            screen.contents = temp;
-            return is;
-
-    // char c;
-    // while (screen.cursor != screen.size() && is >> c)
-    // {
-    //     screen.set(c);
-    //     ++screen.cursor;
-    // }
-    // return is;
+    char c;
+    while (screen.cursor != screen.size() && is >> c)
+    {
+        screen.set(c);
+        ++screen.cursor;
+    }
+    return is;
 }
 
 #endif // _SCREEN_H_
--- cpp_primer/ch16/ex16_15.cpp
+++ cpp_primer/ch16/ex16_15.cpp
@@ -5,11 +5,15 @@ using namespace std;
 int main()
 {
     Screen<5, 5> myScreen('X');
-    myScreen.move(4, 0).set('#').display(cout);
-    cout << '\n';
+    // myScreen.move(4, 0).set('#').display(cout);
+    // cout << '\n';
     // myScreen.display(cout);
-    // cout << myScreen << endl;
+    // // cout << myScreen << endl;
     myScreen.move(0, 0);
     cin >> myScreen;
+    // cout << endl << "aaa :";
+    // aa(myScreen);
+    // myScreen.display(cout);
+    cout << myScreen << endl;
     return 0;
 }
