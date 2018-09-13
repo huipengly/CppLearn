e72694af39431d436c670df756931019a058c936
huipengly <huipengly@gmail.com>
2018-09-13 10:00:35 +0800

    没有调出友元

--- cpp_primer/ch16/Screen.h
+++ cpp_primer/ch16/Screen.h
@@ -6,6 +6,8 @@
 #include <vector>
 
 template <std::string::size_type, std::string::size_type> class Screen;
+template <std::string::size_type X, std::string::size_type Y>
+std::istream &operator>>(const std::istream &, const Screen<X, Y> &);
 
 template <std::string::size_type H, std::string::size_type W>
 class WindowMgr
@@ -18,10 +20,16 @@ private:
     std::vector<Screen<H, W>> screens;
 };
 
+// template <std::string::size_type H, std::string::size_type W>
+// std::ostream &operator<<(const std::ostream &, const Screen<H, W> &);
+
+
 template <std::string::size_type H, std::string::size_type W>
 class Screen
 {
     friend void WindowMgr<H, W>::clear(WindowMgr<H, W>::ScreenIndex);   // Hints: 后面的ScreenIndex也需要定义域符，因为需要指定实例化的WindowMgr
+    // friend std::ostream &operator<<<H, W>(const std::ostream &, const Screen<H, W> &);
+    template <std::string::size_type X, std::string::size_type Y> friend std::istream &operator>>(const std::istream &, const Screen<X, Y> &);      // 任何一个实例均为friend
 
 public:
     using pos = std::string::size_type;
@@ -51,6 +59,32 @@ void WindowMgr<H, W>::clear(ScreenIndex screen_index)     // 需要定义为Wind
         Screen<H, W> &s = screens[screen_index];          //增加别名，提高程序可读性
         s.contents = std::string(s.W * s.H, ' ');
     }
+}
+
+// template <std::string::size_type H, std::string::size_type W>
+// std::ostream &operator<<(const std::ostream &os, const Screen<H, W> &screen)
+// {
+//     os << screen.contents;
+//     return os;
+// }
+
+template <std::string::size_type X, std::string::size_type Y>
+std::istream &operator>>(std::istream &is, Screen<X, Y> &screen)
+{
+
+            char a;
+            is>>a;
+            std::string temp(X*Y, a);
+            screen.contents = temp;
+            return is;
+
+    // char c;
+    // while (screen.cursor != screen.size() && is >> c)
+    // {
+    //     screen.set(c);
+    //     ++screen.cursor;
+    // }
+    // return is;
 }
 
 #endif // _SCREEN_H_
--- cpp_primer/ch16/ex16_15.cpp
+++ cpp_primer/ch16/ex16_15.cpp
@@ -1,13 +1,15 @@
 #include "Screen.h"
 #include <iostream>
-using std::cout;
+using namespace std;
 
 int main()
 {
     Screen<5, 5> myScreen('X');
     myScreen.move(4, 0).set('#').display(cout);
     cout << '\n';
-    myScreen.display(cout);
-    cout << '\n';
+    // myScreen.display(cout);
+    // cout << myScreen << endl;
+    myScreen.move(0, 0);
+    cin >> myScreen;
     return 0;
 }
