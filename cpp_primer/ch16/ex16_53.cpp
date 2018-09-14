cd1dcbb546efc2fe3e59c4ab62b79cf153e1f1af
huipengly <huipengly@gmail.com>
2018-09-14 09:05:59 +0800

    完成练习16.53/16.54

---
+++ cpp_primer/ch16/ex16_53.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void print(ostream &os, const T &t)
+{
+    os << t;
+}
+
+template <typename T, typename ... Args>
+void print(ostream &os, const T &t, const Args& ... rest)
+{
+    os << t << ", ";
+    print(os, rest...);         // Hints:这边是rest...不是..rest
+}
+
+int main(int argc, char const *argv[])
+{
+    print(cout, 1);
+    cout << endl;
+    print(cout, 1, 1.4);
+    cout << endl;
+    print(cout, 1, 3.14, "hello", string("world"), 40);
+    cout << endl;
+    // vector<int> ivec{1, 2, 3};   // 报错
+    // print(cout, 1, ivec);
+    return 0;
+}
