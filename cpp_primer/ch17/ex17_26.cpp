4bfd1670473f4ce1431b6071c6b3a6d787c3b216
huipengly <huipengly@gmail.com>
2018-09-24 17:12:13 +0800

    完成练习17.26

---
+++ cpp_primer/ch17/ex17_26.cpp
@@ -0,0 +1,20 @@
+#include <regex>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string phone = "(\\()?(\\d{3})(\\))?([-. ])?(\\d{3})([-. ])?(\\d{4})";
+    regex r(phone);
+    string s;
+    string fmt = "$2.$5.$7";
+    while (getline(cin, s))
+    {
+        smatch m;
+        regex_search(s, m, r);
+        if (!m.empty())
+            cout << m.prefix().str() << regex_replace(m.suffix().str(), r, fmt) << endl;
+    }
+    return 0;
+}
