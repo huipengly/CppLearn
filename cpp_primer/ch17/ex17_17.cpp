1268f3b97d25311eb1bcc152b04565ef4423bf0d
huipengly <huipengly@gmail.com>
2018-09-24 14:41:22 +0800

    完成练习17.17

---
+++ cpp_primer/ch17/ex17_17.cpp
@@ -0,0 +1,25 @@
+#include <regex>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string pattern = "[^c]ei";
+    pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+    regex r(pattern);
+    smatch results;
+    string in;
+    while (getline(cin, in))
+    {
+        for (sregex_iterator it(in.begin(), in.end(), r), end; it != end; ++it)
+        {
+            auto pos = it->prefix().length();
+            pos = pos > 40 ? pos - 40 : 0;
+            cout << it->prefix().str().substr(pos);
+            cout << "\n\t\t>>> " << it->str() << " <<<\n";
+            cout << it->suffix().str().substr(0, 40) << endl;
+        }
+    }
+    return 0;
+}
