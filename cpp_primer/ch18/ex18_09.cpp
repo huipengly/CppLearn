6e6b1bf28b002c7c1e1457ec9ef49bba90b32eea
huipengly <huipengly@gmail.com>
2018-09-25 11:10:53 +0800

    完成练习18.09

---
+++ cpp_primer/ch18/ex18_09.cpp
@@ -0,0 +1,20 @@
+#include "../ch14/Sales_data.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    Sales_data item1, item2, sum;
+    while (cin >> item1 >> item2)
+    {
+        // cout << item1 << endl;
+        // cout << item2 << endl;
+        try{
+            sum = item1 + item2;
+        } catch (const isbn_mismatch &e){
+            cout << e.what() << ": left isbn(" << e.left << ") right isbn(" << e.right << ")" << endl;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
