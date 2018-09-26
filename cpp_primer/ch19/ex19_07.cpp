0a12b8d89491ec176df33f3be63ebe149d4354f9
huipengly <huipengly@gmail.com>
2018-09-26 09:43:20 +0800

    完成练习19.06

--- cpp_primer/ch19/ex19_07.cpp
+++ cpp_primer/ch19/ex19_07.cpp
@@ -1,10 +1,37 @@
-#include "../ch15/Query.h"
+#include <iostream>
+#include <stdexcept>
+#include "Query.h"
+using namespace std;
+
+// Hints: 为了完成题目，需要将测试用的函数设置为各个类的友元
+void cast_test()
+{
+    AndQuery a1{Query("value1"), Query("value2")};
+    QueryBase &b1 = a1;
+    try{
+        AndQuery a2 = dynamic_cast<AndQuery&>(b1);      // Hints: 这里引用变量转换，<>中要加上&
+        if (typeid(a2) == typeid(a1))
+            cout << "cast success" << endl;
+    } catch (bad_cast &e){
+        cout << "cast failed" << endl;
+        cout << e.what() << endl;
+    }
+    
+    OrQuery o1{Query("value1"), Query("value2")};
+    QueryBase &b2 = o1;
+    try{
+        AndQuery a3 = dynamic_cast<AndQuery&>(b2);      // Hints: 这里引用变量转换，<>中要加上&
+        if (typeid(a3) == typeid(AndQuery))
+            cout << "cast success" << endl;
+    } catch (bad_cast &e){
+        cout << "cast failed" << endl;
+        cout << e.what() << endl;
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
-    // Query q1{"a"}, q2("b");
-    // AndQuery *aqp;
-    Query_base *pb1 = new AndQuery(Query("value1"), Query("value2"));
-    // QueryBase *qb = new AndQuery(q1, q2);
+    cast_test();
     return 0;
 }
