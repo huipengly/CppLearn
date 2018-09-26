fe1597e834cc0debb55eab5fbe76b9a88f9a9198
huipengly <huipengly@gmail.com>
2018-09-26 11:54:53 +0800

    将声明写的更清楚

--- cpp_primer/ch19/ex19_12.cpp
+++ cpp_primer/ch19/ex19_12.cpp
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
 {
     Screen s(2, 2, 'x');
     auto pdata = Screen::data();
+    const Screen::pos Screen::*pdata = Screen::data();
     cout << s.*pdata << endl;
     return 0;
 }
