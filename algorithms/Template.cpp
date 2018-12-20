632caf21a6055694f5d467ffeeb6a4b0b47aea90
huipengly <huipengly@gmail.com>
2018-12-20 21:07:46 +0800

    更新c++模板

--- algorithms/Template.cpp
+++ algorithms/Template.cpp
@@ -5,7 +5,7 @@
  * @version 0.1
  * @date 2018-12-20
  * 
- * @note 包含了常用的库文件，随机数引擎配置好了。
+ * @note 包含了常用的库文件，随机数引擎，测试程序
  */
 #include <iostream>
 #include <vector>
@@ -27,57 +27,78 @@ default_random_engine e(rd());	// 随机数引擎
 // uniform_real_distribution<double> u(0, 10);	// 产生一个[0, 10)的随机实数分布
 // int rand = u(e);	//产生随机数
 
+// 测试用
+auto testMethod(vector<int> &a);
+auto compareMethod(vector<int> &a);
+vector<int> generateRandomArray(int maxSize, int maxValue);
+template <typename T> bool isEqual(T &lhs, T &rhs);
+void test();
+
 int main()
 {
-
+	test();
     return 0;
 }
 
-//// for test
-//void testMethod(vector<int> &a)
-//{
-//	QuickSort(a);
-//}
-//
-//void compareMethod(vector<int> &a)
-//{
-//	sort(a.begin(), a.end());
-//}
-//
-//vector<int> generateRandomArray(int maxSize, int maxValue)
-//{
-//	uniform_int_distribution<unsigned> uValue(0, maxValue);
-//	uniform_int_distribution<unsigned> uSize(0, maxSize);
-//	vector<int> vec(uSize(e));
-//	for (auto &i : vec)
-//		i = uValue(e);
-//	return vec;
-//}
-//
-//bool isEqual(vector<int> &lhs, vector<int> &rhs)
-//{
-//	return lhs == rhs;
-//}
-//
-//void test()
-//{
-//	int testTime = 50000;
-//	int maxSize = 20;
-//	int maxValue = 200;
-//	bool succeed = true;
-//	for (int i = 0; i != testTime; ++i)
-//	{
-//		auto vecTest = generateRandomArray(maxSize, maxValue);
-//		auto vecTrue = vecTest;
-//		auto origin = vecTest;
-//		testMethod(vecTest);
-//		compareMethod(vecTrue);
-//		if (!isEqual(vecTest, vecTrue))
-//		{
-//			succeed = false;
-//			break;
-//		}
-//	}
-//
-//	cout << (succeed ? "passed" : "BUG!") << endl;
-//}
+// 以下为测试用代码
+auto testMethod(vector<int> &a)
+{
+	// 测试方法
+}
+
+auto compareMethod(vector<int> &a)
+{
+	// 对的方法
+}
+
+// 产生随机大小，数目的数组
+vector<int> generateRandomArray(int maxSize, int maxValue)
+{
+	uniform_int_distribution<unsigned> uValue(0, maxValue);
+	uniform_int_distribution<unsigned> uSize(0, maxSize);
+	vector<int> vec(uSize(e));
+	for (auto &i : vec)
+		i = uValue(e);
+	return vec;
+}
+
+// 判断结果是否相等
+template <typename T>
+bool isEqual(T &lhs, T &rhs)
+{
+	return lhs == rhs;
+}
+
+void test()
+{
+	int testTime = 50000;
+	int maxSize = 20;
+	int maxValue = 200;
+	bool succeed = true;
+	for (int i = 0; i != testTime; ++i)
+	{
+		auto TestData = generateRandomArray(maxSize, maxValue);
+		auto TrueData = TestData;
+		auto OriginData = TestData;
+
+		// 无返回类型的判断
+		testMethod(TestData);
+		compareMethod(TrueData);
+		if (!isEqual(TestData, TrueData))
+		{
+			succeed = false;
+			break;
+		}
+
+		/// 有返回类型的判断
+		//auto TestResult = testMethod(TestData);
+		//auto CompareResult = compareMethod(TrueData);
+		//if (!isEqual(TestResult, CompareResult))
+		//{
+		//	succeed = false;
+		//	break;
+		//}
+	}
+
+	cout << (succeed ? "passed" : "BUG!") << endl;
+}
