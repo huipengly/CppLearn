67d68e6124fc9966146fd375106712009fae855b
huipengly <huipengly@gmail.com>
2018-12-21 10:26:17 +0800

    完成矩阵旋转90度

---
+++ algorithms/array/RotateMatrix.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file Template.cpp
+ * @author huipengly
+ * @brief 方阵旋转90度打印
+ * @version 0.1
+ * @date 2018-12-20
+ * 
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <random>
+#include <queue>
+#include <stack>
+#include <memory>
+
+using namespace std;
+
+std::random_device rd;			// 用来播种
+default_random_engine e(rd());	// 随机数引擎
+// uniform_int_distribution<int> u(0, 10);	// 产生一个[0, 10]的随机整数分布
+// uniform_real_distribution<double> u(0, 10);	// 产生一个[0, 10)的随机实数分布
+// int rand = u(e);	//产生随机数
+
+// shared_ptr<int> pint = make_shared<int>();   // 智能指针&申请内存
+
+// 测试用
+auto testMethod(vector<int> &a);
+auto compareMethod(vector<int> &a);
+vector<int> generateRandomArray(int maxSize, int maxValue);
+template <typename T> bool isEqual(T &lhs, T &rhs);
+void test();
+
+// 打印二维vector
+template <typename T> ostream& operator<<(ostream &os, const vector<vector<T>> &matrix);
+
+void rotateCycle(vector<vector<int>> &arr, int topLeft, int bottomRight)
+{
+	for (int i = 0; i != bottomRight - topLeft; ++i)
+	{
+		std::swap(arr[topLeft][topLeft + i], arr[bottomRight - i][topLeft]);
+		std::swap(arr[bottomRight - i][topLeft], arr[bottomRight][bottomRight - i]);
+		std::swap(arr[bottomRight][bottomRight - i], arr[topLeft + i][bottomRight]);
+	}
+}
+
+void RotateMatrix(vector<vector<int>> &arr)
+{
+	int size = arr.size();
+	int topLeft = 0, bottomRight = size - 1;
+	while (topLeft <= bottomRight)
+	{
+		rotateCycle(arr, topLeft++, bottomRight--);
+	}
+}
+
+int main()
+{
+	//test();
+	vector<vector<int>> matrix { {1, 2, 3, 4}, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 }};
+	RotateMatrix(matrix);
+	cout << matrix;
+    return 0;
+}
+
+// 以下为测试用代码
+auto testMethod(vector<int> &a)
+{
+	// 测试方法
+}
+
+auto compareMethod(vector<int> &a)
+{
+	// 对的方法
+}
+
+// 产生随机大小，数目的数组
+vector<int> generateRandomArray(int maxSize, int maxValue)
+{
+	uniform_int_distribution<unsigned> uValue(0, maxValue);
+	uniform_int_distribution<unsigned> uSize(0, maxSize);
+	vector<int> vec(uSize(e));
+	for (auto &i : vec)
+		i = uValue(e);
+	return vec;
+}
+
+// 判断结果是否相等
+template <typename T>
+bool isEqual(T &lhs, T &rhs)
+{
+	return lhs == rhs;
+}
+
+void test()
+{
+	int testTime = 50000;
+	int maxSize = 20;
+	int maxValue = 200;
+	bool succeed = true;
+	for (int i = 0; i != testTime; ++i)
+	{
+		auto TestData = generateRandomArray(maxSize, maxValue);
+		auto TrueData = TestData;
+		auto OriginData = TestData;
+
+#define NO_RETURN_VALUE
+//#define HAS_RETURN_VALUE
+
+#ifdef NO_RETURN_VALUE
+		// 无返回类型的判断
+		testMethod(TestData);
+		compareMethod(TrueData);
+		if (!isEqual(TestData, TrueData))
+		{
+			succeed = false;
+			break;
+		}
+#endif
+
+#ifdef HAS_RETURN_VALUE
+		// 有返回类型的判断
+		auto TestResult = testMethod(TestData);
+		auto CompareResult = compareMethod(TrueData);
+		if (!isEqual(TestResult, CompareResult))
+		{
+			succeed = false;
+			break;
+		}
+#endif
+	}
+
+	cout << (succeed ? "passed" : "BUG!") << endl;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, const vector<vector<T>> &matrix)
+{
+	if (matrix.empty())
+		return os;
+	auto rowSize = matrix.size();			// 行大小
+	auto colSize = matrix[0].size();		// 列大小
+	for (int i = 0; i != rowSize; ++i)
+	{
+		for (int j = 0; j != colSize; ++j)
+		{
+			os << matrix[i][j] << ", \t";
+		}
+		os << "\n";
+	}
+}
