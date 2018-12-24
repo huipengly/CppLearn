663355801d2958675718249d61ed4523296ac1b4
huipengly <huipengly@gmail.com>
2018-12-24 15:27:09 +0800

    完成单链表判断有无环

---
+++ leetcode/141. Linked List Cycle/141. Linked List Cycle.cpp
@@ -0,0 +1,203 @@
+/**
+ * @file Template.cpp
+ * @author huipengly
+ * @brief 写笔试题目的c++模板
+ * @version 0.1
+ * @date 2018-12-20
+ * 
+ * @note 包含了常用的库文件，随机数引擎，测试程序
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
+#include <random>
+#include <queue>
+#include <stack>
+#include <memory>
+
+using namespace std;
+
+std::random_device rd;			// 用来播种
+default_random_engine e(rd());	// 随机数引擎
+// uniform_int_distribution<int> u(0, 10);	// 产生一个[0, 10]的随机整数分布
+// uniform_real_distribution<double> u(0, 10);	// 产生一个[0, 10)的随机实数分布
+// int rand = u(e);	//产生随机数
+
+// shared_ptr<int> pint = make_shared<int>();   // 智能指针&申请内存
+
+// 链表结构
+struct ListNode {
+   int val;
+	ListNode *next;
+   ListNode(int x) : val(x), next(NULL) {}
+};
+
+// 链表结构，使用智能指针
+struct ListNodeUseSharedPtr {
+   int val;
+   shared_ptr<ListNode> next;		// 自动回收内存
+   ListNodeUseSharedPtr(int x) : val(x), next(NULL) {}
+};
+
+//// 双向链表结构
+// struct DoublyListNode {
+//     int val;
+// 	shared_ptr<DoublyListNode> pre;
+//     shared_ptr<DoublyListNode> next;
+// 	//ListNode *pre;
+// 	//ListNode *next;
+// 	DoublyListNode(int x) : val(x), pre(NULL), next(NULL) {}
+// };
+
+// 测试用
+auto testMethod(vector<int> &a);
+auto compareMethod(vector<int> &a);
+vector<int> generateRandomArray(int maxSize, int maxValue);
+template <typename T> bool isEqual(T &lhs, T &rhs);
+void test();
+
+// 打印二维vector
+template <typename T> ostream& operator<<(ostream &os, const vector<vector<T>> &matrix);
+ostream& operator<<(ostream &os, const ListNode *head);
+
+// 使用一个set记录走过的节点，如果之后的节点是走过的节点，则这个点就是入环点
+class Solution1 {
+public:
+	bool hasCycle(ListNode *head) {
+		unordered_set<ListNode *> node;
+		auto *curr = head;
+		while (curr != nullptr)
+		{
+			if (node.count(curr))
+				return true;
+			node.insert(curr);
+			curr = curr->next;
+		}
+		return false;
+	}
+};
+
+// 快慢指针，如果有环，则快指针一定和慢指针相遇
+class Solution2 {
+public:
+	bool hasCycle(ListNode *head) {
+		auto *slow = head, *fast = head;
+		while (slow != nullptr && fast != nullptr && fast->next != nullptr)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+			if (slow == fast)
+				return true;
+		}
+		return false;
+	}
+};
+
+int main()
+{
+
+    return 0;
+}
+
+// 以下为测试用代码
+auto testMethod(vector<int> &a)
+{
+	// 测试方法
+}
+
+auto compareMethod(vector<int> &a)
+{
+	// 对的方法
+}
+
+// 产生随机大小，数目的数组
+vector<int> generateRandomArray(int maxSize, int maxValue)
+{
+	uniform_int_distribution<unsigned> uValue(0, maxValue);
+	uniform_int_distribution<unsigned> uSize(0, maxSize);
+	vector<int> vec(uSize(e));
+	for (auto &i : vec)
+		i = uValue(e);
+	return vec;
+}
+
+// 判断结果是否相等
+template <typename T>
+bool isEqual(T &lhs, T &rhs)
+{
+	return lhs == rhs;
+}
+
+void test()
+{
+	int testTime = 50000;
+	int maxSize = 20;
+	int maxValue = 200;
+	bool succeed = true;
+	for (int i = 0; i != testTime; ++i)
+	{
+		auto TestData = generateRandomArray(maxSize, maxValue);
+		auto TrueData = TestData;
+		auto OriginData = TestData;
+
+#define NO_RETURN_VALUE
+//#define HAS_RETURN_VALUE
+
+#ifdef NO_RETURN_VALUE
+		// 无返回类型的判断
+		testMethod(TestData);
+		compareMethod(TrueData);
+		if (!isEqual(TestData, TrueData))
+		{
+			succeed = false;
+			break;
+		}
+#endif
+
+#ifdef HAS_RETURN_VALUE
+		// 有返回类型的判断
+		auto TestResult = testMethod(TestData);
+		auto CompareResult = compareMethod(TrueData);
+		if (!isEqual(TestResult, CompareResult))
+		{
+			succeed = false;
+			break;
+		}
+#endif
+	}
+
+	cout << (succeed ? "passed" : "BUG!") << endl;
+}
+
+template <typename T>
+ostream& operator<<(ostream &os, const vector<vector<T>> &matrix)
+{
+	if (matrix.empty())
+		return os;
+	auto rowSize = matrix.size();			// 行大小
+	auto colSize = matrix[0].size();		// 列大小
+	for (int i = 0; i != rowSize; ++i)
+	{
+		for (int j = 0; j != colSize; ++j)
+		{
+			os << matrix[i][j] << ", \t";
+		}
+		os << "\n";
+	}
+}
+
+ostream &operator<<(ostream & os, const ListNode * head)
+{
+	auto *curr = head;
+	while (curr != nullptr)
+	{
+		os << curr->val << " ";
+		curr = curr->next;
+	}
+	return os;
+}
